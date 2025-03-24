33efef5ef00379b91c5b9624c9080d1d343cf341
Techwin Labs LLP <techwinlabs@gmail.com>
2025-03-24 10:57:53 +0530

    Code updated

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -3,13 +3,13 @@ import ImageModal from "../ImageModal";
 const ImageGallery = ({ storageImages }: { storageImages: string[] }) => {
   return (
     <div className="flex min-h-[480px] max-h-[480px] max-md:min-h-[320px] ">
-      <div className="w-[50%] max-md:w-full">
+      <div className="for-fix-image w-[50%] max-md:w-full">
         <ImageModal
           className="rounded-l-[16px] w-full h-full object-cover max-md:rounded-[16px]"
           url={storageImages[0]}
         />
       </div>
-      <div className="w-[25%] mx-[12px] max-lg:mr-[0] flex flex-col gap-[12px] max-lg:w-[50%] max-lg:ml-[12px] max-md:hidden">
+      <div className="for-fix-image w-[25%] mx-[12px] max-lg:mr-[0] flex flex-col gap-[12px] max-lg:w-[50%] max-lg:ml-[12px] max-md:hidden">
         <ImageModal
           className="w-full h-[49%] object-cover max-lg:min-h-[49%]"
           url={storageImages[1]}
@@ -21,7 +21,7 @@ const ImageGallery = ({ storageImages }: { storageImages: string[] }) => {
           />
         )}
       </div>
-      <div className="w-[25%]  flex flex-col gap-[12px] max-lg:hidden">
+      <div className="for-fix-image for-fix-image w-[25%]  flex flex-col gap-[12px] max-lg:hidden">
         {storageImages[3] && (
           <ImageModal
             className="w-full h-[49%] rounded-tr-[16px] object-cover"
